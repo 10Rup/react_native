64946af8783cd928240539c70f5ad5a2b52bb643
Rup Mandal <104366033+10Rup@users.noreply.github.com>
2025-02-16 19:42:37 +0530

    day1

--- my-app/app/menu.jsx
+++ my-app/app/menu.jsx
@@ -1,25 +1,37 @@
-import { Appearance, StyleSheet, Platform, SafeAreaView, ScrollView, FlatList, View, Text, Iamge } from "react-native";
+import { Appearance, StyleSheet, Platform, SafeAreaView, ScrollView, FlatList, View, Text, Image } from "react-native";
 import {Colors} from "@/constants/Colors";
 import {MENU_ITEMS} from "@/constants/MenuItems";
-
+import MENU_IMAGES from "@/constants/MenuImages"
 
 export default function Menu_Screen() {
     const colorScheme = Appearance.getColorScheme();
     const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
     const styles = createStyleSheet(theme, colorScheme);
     const Container = Platform.OS === "web"? ScrollView : SafeAreaView;
-    
+    const SeparatorComponent= <View style={styles.separator}></View>
+    const headerComponent =<Text>Top Of the List</Text>
+    const footerComponent =<Text>End Of the List</Text>
     return (
         <Container>
             <FlatList 
                 data={MENU_ITEMS}
+                ListEmptyComponent={<Text>No Menu Found</Text>}
                 keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) =>{
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.contentContainer}
+                ItemSeparatorComponent={SeparatorComponent}
+                ListHeaderComponent={headerComponent}
+                ListFooterComponent={footerComponent}
+                ListFooterComponentStyles={styles.footer}
+                renderItem={({item}) =>(
                     <View>
-                        <Text>{item}</Text>
+                        <View>
+                        <Text>{item.title}</Text>
+                        <Text>{item.description}</Text>
+                        </View>
+                        <Image source={MENU_IMAGES[item.id-1]} style={styles.image}></Image>
                     </View>
-                }}
-            >
+                )}>
 
             </FlatList>
 
@@ -31,6 +43,27 @@ export default function Menu_Screen() {
 
 function createStyleSheet(theme, colorScheme){
     return StyleSheet.create({
+        contentContainer:{
+            paddingTop:20,
+            paddingBottom:20,
+            paddingRight:10,
+            paddingLeft:10,
+            // paddingHorizontal:10,
+            // backgroundColor: theme.background,
+        },
+        image:{
+            width:'100%',
+            heigth:'100%',
+            flex:1,
+            justifyContent:'center',
+        },
+        separator:{
+            height:5,
+            backgroundColor:'rgb(73, 81, 73)'
+        },
+        footer:{
+            marginHorizontal:'auto'
+        }
 
     })
 }
---
+++ my-app/constants/MenuImages.js
@@ -0,0 +1,14 @@
+import expresso from "@/assets/images/expresso-img.jpg"
+import americano from "@/assets/images/americano-img.jpg"
+import cappuccino from "@/assets/images/cappuccino-img.jpg"
+import latte from "@/assets/images/latte-img.jpg"
+import tea from "@/assets/images/tea-img.jpg"
+
+
+export default [
+    expresso,
+    americano,
+    latte,
+    cappuccino,
+    tea
+]
